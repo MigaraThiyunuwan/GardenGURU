0bc67272e88cdfd70a8e498d11f65da48b4f8978
MigaraThiyunuwan <86980643+MigaraThiyunuwan@users.noreply.github.com>
2023-07-17 19:04:35 +0530

    title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
     <meta charset="utf-8">
-    <title>Gardener - Gardening Website Template</title>
+    <title>GardenGURU</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
