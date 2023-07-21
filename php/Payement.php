10297ee691f63e8a6064061cae17867c83fad2ba
malki <90499866+Malkimadhu@users.noreply.github.com>
2023-07-21 15:26:09 +0530

    new changes in payment page

--- php/Payement.php
+++ php/Payement.php
@@ -17,6 +17,7 @@
     <!-- Template Stylesheet -->
     <link href="../css/style.css" rel="stylesheet">
     <link href="../css/Payement.css" rel="stylesheet">
+    
 
 </head>
 
@@ -142,8 +143,9 @@
 
     </div>
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+   
+     <!-- Footer Start -->
+     <div class="container-fluid bg-dark text-light footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
