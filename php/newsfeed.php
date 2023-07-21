fbec7bfa4ce3dd5e55677830d5a7259034739242
Nipuni Navodya <138982836+Navo626@users.noreply.github.com>
2023-07-21 11:48:56 +0530

    copyright

--- php/newsfeed.php
+++ php/newsfeed.php
@@ -275,7 +275,20 @@
         </div>
     </div>
     <!-- Footer End -->
+         
+    
+    <!-- Copyright Start -->
+ <div class="container-fluid copyright py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="border-bottom" href="index.php">GardenGURU</a>, All Right Reserved.
+                </div>
              
+            </div>
+        </div>
+    </div>
+    <!-- Copyright End -->
 
         <script src="../js/script1.js" async defer></script>
         <script src="../js/main.js" async defer></script>
