5820cf37cd7c229badb8e1d68fb938d90d83d53a
Nipuni Navodya <138982836+Navo626@users.noreply.github.com>
2023-07-21 10:17:05 +0530

    news1

---
+++ php/newsfeed.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="main1.css">
+        <link rel="stylesheet" href="style.css">
+
+        <link href="bootstrap.min.css" rel="stylesheet">
+        <!-- google fonts -->
+        <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Raleway:wght@300;400;500;700;900&display=swap"
+            rel="stylesheet">
+            
+        <!-- fontawesome -->
+        <script src="https://kit.fontawesome.com/dbed6b6114.js"
+            crossorigin="anonymous"></script>
+
+            
+        
+    </head>
+    <body>
+
+        <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
+        <a href="index-2.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5" >
+        <img src="images/logo.png" style="width:220px;height:50px;">
+            <!-- <h1 class="m-0">Garden<B>GURU</B></h1> -->
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index-2.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="service.html" class="nav-item nav-link">Services</a>
+                <a href="project.html" class="nav-item nav-link">Projects</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu bg-light m-0">
+                        <a href="feature.html" class="dropdown-item">Features</a>
+                        <a href="quote.html" class="dropdown-item">Free Quote</a>
+                        <a href="team.html" class="dropdown-item">Our Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                        <a href="404.html" class="dropdown-item">404 Page</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <!-- <a href="#" class="btn btn-primary py-4 px-lg-4 rounded-0 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a> -->
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+        <section class="banner">
+            <div class="banner-main-content">
+                <h2>GET THE LATEST NEWS FOR PLANT ENTHUSIASTS!</h2>
+                <!-- <h3</h3> -->
+
+                
+
+                <div class="current-news-head">
+                    <h3>Indoor Plant Trends Inspire Home Decor
+                        <span>by Kamal Edirisinghe</span></h3>
+
+                    <h3>Innovative Soil Regeneration Techniques Restore Degraded Lands
+                        <span>by D.B.Seneviratne</span></h3>
+
+                    <h3>Record-Breaking Plant Exhibition Draws Gardeners Globally<span>by Victor Malinda</span></h3>
+
+                    <h3>Biosecurity Measures Implemented to Protect Plant Health<span>by Gamini Perera</span></h3>
+                </div>
+            </div>
+
+            <div class="banner-sub-content">
+                <div class="hot-topic">
+                    <img src="images/Watersaving_Tips_Image_Overlay_V2-1.webp" alt>
+
+                    <div class="hot-topic-content">
+                        <h2>Revolutionary Water-Saving Technique Gains Traction in Agriculture</h2>
+
+<!--                         <h3>New Topic 1</h3>
+ -->                        <p>A breakthrough water-saving technique developed by researchers at a leading agricultural institute has caught the attention of farmers worldwide. Using soil moisture sensors and precision irrigation, this innovative approach has shown a significant reduction in water usage while increasing crop yields. </p>
+                        <a href="#">Read More</a>
+                    </div>
+                </div>
+
+                <div class="hot-topic">
+                    <img src="images/12-urban-ag.png" alt>
+
+                    <div class="hot-topic-content">
+                        <h2>Urban Gardening Movement Flourishing in Cities</h2>
+
+                        
+                        <p>In response to the growing interest in sustainable living and green spaces, urban gardening is witnessing a surge in popularity. More and more city dwellers are embracing container gardening, rooftop gardens, and community plots to grow fresh produce and beautify their surroundings. </p>
+                        <a href="#">Read More</a>
+                    </div>
+                </div>
+
+                <div class="hot-topic">
+                    <img src="images/imagesss.jpeg" alt>
+
+                    <div class="hot-topic-content">
+                        <h2>Plant-Based Agriculture Leads to Reduced Carbon Emissions</h2>
+
+                        
+                        <p>A recent study conducted by an environmental research organization revealed that transitioning from animal-based agriculture to plant-based agriculture can significantly reduce carbon emissions. The study highlights the positive impact of adopting a plant-based diet, not only for personal health but also for the environment.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </div>
+
+                <div class="hot-topic">
+                    <img src="images/amazon.jpg" alt>
+
+                    <div class="hot-topic-content">
+                        <h2>Newly Discovered Plant Species in the Amazon Rainforest</h2>
+
+                        
+                        <p>A team of biologists exploring the depths of the Amazon rainforest has stumbled upon a previously unknown plant species with unique medicinal properties. Initial studies suggest that extracts from this plant could have potential applications in treating certain ailments. However, conservation efforts are already underway to protect this newfound treasure from potential threats.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <hr>
+
+        <main>
+            <section class="main-container-left">
+                <h2>Top Stories</h2>
+                <div class="container-top-left">
+                    <article>
+                        <img src="images/GettyImages-1318237749.webp">
+
+                        <div>
+                            <h3>AI-Powered Farming Tools Enhancing Productivity</h3>
+
+                            <p>Artificial Intelligence (AI) continues to revolutionize the agricultural landscape with the introduction of AI-powered farming tools. From automated crop monitoring systems to drones equipped with AI algorithms for precision agriculture, these technologies are streamlining farming processes, optimizing resource usage, and maximizing yields.</p>
+
+                            <a href="#">Read More <span>>></span></a>
+                        </div>
+                    </article>
+                </div>
+
+                <div class="container-bottom-left">
+                    <article>
+                        <img src="images/csm_NRP_1551_d35b1af732.webp">
+                        <div>
+                            <h3>Global Seed Vault Celebrates Milestone</h3>
+                            <p>The Svalbard Global Seed Vault, often referred to as the "Doomsday Vault," recently reached a remarkable milestone by storing its one-millionth seed sample. Located in the Arctic permafrost, the vault serves as a vital resource for preserving plant genetic diversity and safeguarding against potential food crises, climate-related disasters, and other threats to global agriculture.</p>
+
+                            <a href="#">Read More <span>>></span></a>
+                        </div>
+                    </article>
+
+                    <article>
+                        <img src="images/12-urban-ag.png">
+                        <div>
+                            <h3>Bioengineered Superplants Show Promising Results</h3>
+                            <p>Scientists working on bioengineering projects have made significant strides in developing "superplants" that exhibit enhanced resilience to diseases, pests, and adverse environmental conditions. By incorporating desirable traits from various plant species, these bioengineered crops have demonstrated the potential to increase food security and sustainability, though they also raise ethical and ecological concerns.</p>
+
+                            <a href="#">Read More <span>>></span></a>
+                        </div>
+                    </article>
+                </div>
+            </section>
+
+            <section class="main-container-right">
+                <h2>Latest Stories</h2>
+
+                <article>
+                    <h4>just in </h4>
+                    <div>
+                        <h2>Vertical Farming Takes Off in Urban Centers</h2>
+
+                        <p>Vertical farming, an innovative method of growing crops in stacked layers, is gaining momentum in urban centers worldwide. With limited arable land in cities, vertical farms offer a solution to produce fresh, pesticide-free vegetables and herbs locally.</p>
+
+                        <a href="#">Read More <span>>></span></a>
+                    </div>
+                    <img src="images/vertical-farming-cities-dassault-systemes.jpg">
+                </article>
+
+                <article>
+                    <h4>just in </h4>
+                    <div>
+                        <h2>New Legislation Promotes Pollinator-Friendly Farming Practices</h2>
+
+                        <p>In response to the declining populations of bees and other pollinators, several countries have introduced legislation to promote pollinator-friendly farming practices. These measures encourage the use of native wildflowers as cover crops, reduced pesticide application, and the establishment of pollinator-friendly habitats on farmland.</p>
+
+                        <a href="#">Read More <span>>></span></a>
+                    </div>
+                    <img src="images/221017-bee-better-pollinator-certifications-farms-sustainability-pesticides-7-Doug-Crabtree-in-sunflower-crop_by-Jennifer-Hopwood-Xerces-Society.jpg">
+                </article>
+
+                <article>
+                    <h4>just in </h4>
+                    <div>
+                        <h2>Rise of AI-Powered Plant Identification Apps</h2>
+
+                        <p>Plant identification apps powered by AI are becoming increasingly popular among gardening enthusiasts and nature lovers. These apps utilize machine learning algorithms to identify plant species from images taken with smartphones.</p>
+
+                        <a href="#">Read More <span>>></span></a>
+                    </div>
+                    <img src="images/PictureThis-identification.webp">
+                </article>
+
+                <article>
+                    <h4>just in </h4>
+                    <div>
+                        <h2>Organic Farming Receives Government Support</h2>
+
+                        <p>Governments worldwide are providing increased support and incentives for organic farming practices. Recognizing the importance of sustainable agriculture, subsidies, grants, and educational programs are being introduced to encourage farmers to transition to organic methods.</p>
+
+                        <a href="#">Read More <span>>></span></a>
+                    </div>
+                    <img src="images/selecting-best-maize-variety.jpg">
+                </article>
+
+                <article>
+                    <h4>just in </h4>
+                    <div>
+                        <h2>Hybrid Varieties Show Promise in Climate-Resilient Farming</h2>
+
+                        <p>With climate change posing challenges to traditional crop varieties, farmers are turning to hybrid plant breeds that exhibit greater resilience to extreme weather conditions. These climate-resilient hybrids have demonstrated improved tolerance to heat, drought, and flooding, offering hope for ensuring food security amidst changing climatic patterns.</p>
+
+                        <a href="#">Read More <span>>></span></a>
+                    </div>
+                    <img src="images/rice_plant.webp">
+                </article>
+            </section>
+        </main>
+
+        <!-- <footer>
+             -->
+
+             
+
+        <script src="script1.js" async defer></script>
+        <script src="main.js" async defer></script>
+    </body>
+</html>
