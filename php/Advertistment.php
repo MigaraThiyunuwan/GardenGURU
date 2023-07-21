0d72620da919fa1b2707288fdb263fa0c1c5e677
malki <90499866+Malkimadhu@users.noreply.github.com>
2023-07-21 14:51:42 +0530

    add blog to nav bar in advertisement page

--- php/Advertistment.php
+++ php/Advertistment.php
@@ -42,10 +42,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="index-2.html" class="nav-item nav-link active">Home</a>
-                <a href="AboutUs.php" class="nav-item nav-link">About</a>
+                <a href="../index.php" class="nav-item nav-link active">Home</a>
+                <a href="../php/AboutUs.php" class="nav-item nav-link">About</a>
                 <a href="service.html" class="nav-item nav-link">Services</a>
                 <a href="project.html" class="nav-item nav-link">Projects</a>
+                <a href="../php/blog.php" class="nav-item nav-link">Blog</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu bg-light m-0">
@@ -56,7 +57,7 @@
                         <a href="404.html" class="dropdown-item">404 Page</a>
                     </div>
                 </div>
-                <a href="ContactUs.php" class="nav-item nav-link">Contact</a>
+                <a href="../php/ContactUs.php" class="nav-item nav-link">Contact</a>
             </div>
             <!-- <a href="#" class="btn btn-primary py-4 px-lg-4 rounded-0 d-none d-lg-block">Get A Quote<i class="fa fa-arrow-right ms-3"></i></a> -->
         </div>
