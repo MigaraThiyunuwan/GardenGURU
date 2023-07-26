1735f4c3d448f3f314de3724cb11a7577a8fd7ec
malki <90499866+Malkimadhu@users.noreply.github.com>
2023-07-26 12:01:27 +0530

    new payement .php

--- php/Payement.php
+++ php/Payement.php
@@ -1,3 +1,30 @@
+<?php
+	// Authorisation details.
+	$username = "malkimadhubhashini2001@gmail.com";
+	$hash = "ef03895199d941541de24f74059f37d68b15bcd1e68126ed3b0a7ec6f3d42265";
+
+	// Config variables. Consult http://api.txtlocal.com/docs for more info.
+	$test = "0";
+
+	// Data for text message. This is the text message data.
+	$sender = "API Test"; // This is who the message appears to be from.
+	$numbers = "44777000000"; // A single number or a comma-seperated list of numbers
+	$message = "This is a test message from the PHP API script.";
+	// 612 chars or less
+	// A single number or a comma-seperated list of numbers
+	$message = urlencode($message);
+	$data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+	$ch = curl_init('https://api.txtlocal.com/send/?');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch); // This is the result from the API
+	curl_close($ch);
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
