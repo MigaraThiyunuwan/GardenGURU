f872ae59c7806a83a9064c1c43f988e0b349b159
Dharani-29999 <gunasekara.dharani29@gmail.com>
2023-07-30 20:25:53 +0530

    cart progress

--- php/mycart.php
+++ php/mycart.php
@@ -180,15 +180,15 @@
                         <th scope="col">Item Name</th>
                         <th scope="col">Item Price</th>
                         <th scope="col">Quantity</th>
+                        <th scope="col">Total</th>
+
                         <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody class="text-center">
 
                     <?php
-
-                        
-                     $total=0;
+                        $total=0;
                     if (isset($_SESSION['cart'])) {
                         $serialNo = 1;
                         foreach ($_SESSION['cart'] as $key => $value) {
@@ -196,8 +196,11 @@
                             echo "<tr>
                                     <td>{$serialNo}</td>
                                     <td>{$value['Item_Name']}</td>
-                                    <td>{$value['Price']}</td>
-                                    <td><input class='text-center' type='number' value='$value[Quantity]' min='1' max='10'></td>
+                                    <td>{$value['Price']}<input type='hidden' class='iprice' value='{$value['Price']}'></td>
+
+                                    <td><input class='text-center iquantity' type='number' value='$value[Quantity]' min='1' max='10'></td>
+                                    <td class='itotal'></td>
+
                                     <td>
                                         <form action='manage_cart.php' method='POST'>
                                         <button name='Remove_Item' class='btn btn-sm btn-outline-danger'>Remove</button>
@@ -208,7 +211,6 @@
                             $serialNo++;
                         }
                     }
-
                     ?>
 
                 </tbody>
@@ -251,10 +253,12 @@
 
 
 
+<script>
+    
+</script>
 
 
-
-
+var iprice =
 
 
 
