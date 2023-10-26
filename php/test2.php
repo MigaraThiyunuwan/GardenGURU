e4783e9e4175c4b4d4b4f5f36c932c9e1b290844
LashanSachintha <84861849+LashanSachintha@users.noreply.github.com>
2023-10-26 19:31:23 +0530

    Update test2.php
    day1

--- php/test2.php
+++ php/test2.php
@@ -99,6 +99,49 @@
 /////////////////////////////////////////////// Lashan ///////////////////////////////////////////////////////////////////
 
 
+if (isset($_POST["submit"])) {
+    $uploadDirectory = "uploads/"; // The directory where you want to store uploaded files
+    $targetFile = $uploadDirectory . basename($_FILES["file"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Check if the file already exists
+    if (file_exists($targetFile)) {
+        echo "File already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check file size (adjust the size limit as needed)
+    if ($_FILES["file"]["size"] > 5000000) {
+        echo "File is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow only specific file formats (e.g., you can change this to allow other file types)
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+        echo "Only JPG, JPEG, and PNG files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "File was not uploaded.";
+    } else {
+        // If everything is fine, attempt to move the file to the specified directory
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+            echo "The file " . basename($_FILES["file"]["name"]) . " has been uploaded.";
+        } else {
+            echo "There was an error uploading your file.";
+        }
+    }
+}
+
+
+
+
+
+
+
 
 
 
