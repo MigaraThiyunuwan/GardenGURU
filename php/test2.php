e8199301d70aef282d83fc3bf04d037f61feb8b5
LashanSachintha <84861849+LashanSachintha@users.noreply.github.com>
2023-11-13 10:53:35 +0530

    test2222

--- php/test2.php
+++ php/test2.php
@@ -356,6 +356,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
+    $targetDir = "uploads/"; // Directory where you want to store the uploaded documents
+    $targetFile = $targetDir . basename($_FILES["file"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Check if the file is a document
+    if ($imageFileType != "pdf" && $imageFileType != "doc" && $imageFileType != "docx") {
+        echo "Sorry, only PDF, DOC, and DOCX files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if the file already exists
+    if (file_exists($targetFile)) {
+        echo "Sorry, the file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        // If everything is ok, try to upload the file
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+            echo "The file " . basename($_FILES["file"]["name"]) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
 
 
 
